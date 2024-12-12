8f374603b66c7300ba6c4dee53bbfe6bb4250807
GeniNik <eugnik21@htl-shkoder.com>
2024-12-12 11:04:24 +0100

    Divide Method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -13,6 +13,11 @@ public class Calculator {
         return a * b;
     }
 
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        return a / b;
+    }
+
 
 }
 
